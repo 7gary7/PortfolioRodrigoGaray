92fa2472ebf0df13e553d08b87b6531ff04171d7
RodrigoGary <rodri_25193@hotmail.com>
2021-01-04 16:50:57 -0300

    cambio de idioma:   .eslintrc.js cambio de idioma:   .gitignore cambio de idioma:   .prettierignore cambio de idioma:   .prettierrc cambio de idioma:   LICENSE.md cambio de idioma:   README.md cambio de idioma:   examples/example.gif cambio de idioma:   examples/example.png cambio de idioma:   gatsby-config.js cambio de idioma:   package-lock.json cambio de idioma:   package.json cambio de idioma:   src/components/About/About.jsx cambio de idioma:   src/components/App.jsx cambio de idioma:   src/components/Contact/Contact.jsx cambio de idioma:   src/components/Footer/Footer.jsx cambio de idioma:   src/components/GithubButtons/GithubButtons.jsx cambio de idioma:   src/components/Hero/Hero.jsx cambio de idioma:   src/components/Image/AboutImg.jsx cambio de idioma:   src/components/Image/ProjectImg.jsx cambio de idioma:   src/components/Projects/Projects.jsx cambio de idioma:   src/components/Title/Title.jsx cambio de idioma:   src/context/context.js cambio de idioma:   src/html.js cambio de idioma:   src/images/favicon.png cambio de idioma:   src/images/garyYcris.jpg cambio de idioma:   src/images/pipperman.jpg cambio de idioma:   src/images/profile.jpg cambio de idioma:   src/images/project.jpg cambio de idioma:   src/mock/data.js cambio de idioma:   src/pages/404.js cambio de idioma:   src/pages/index.js cambio de idioma:   src/style/abstracts/_helpers.scss cambio de idioma:   src/style/abstracts/_mixins.scss cambio de idioma:   src/style/abstracts/_variables.scss cambio de idioma:   src/style/base/_base.scss cambio de idioma:   src/style/base/_typography.scss cambio de idioma:   src/style/components/_buttons.scss cambio de idioma:   src/style/layout/_footer.scss cambio de idioma:   src/style/layout/_sections.scss cambio de idioma:   src/style/main.scss cambio de idioma:   src/style/sections/_about.scss cambio de idioma:   src/style/sections/_contact.scss cambio de idioma:   src/style/sections/_hero.scss cambio de idioma:   src/style/sections/_projects.scss cambio de idioma:   src/style/vendor/css/font-awesome.min.css cambio de idioma:   src/style/vendor/fonts/fontawesome-webfont.woff2 cambio de idioma:   yarn.lock

---
+++ src/mock/data.js
@@ -0,0 +1,80 @@
+import { nanoid } from 'nanoid';
+
+// HEAD DATA
+export const headData = {
+  title: 'Rodrigo Garay', // e.g: 'Name | Developer'
+  lang: 'es', // e.g: en, es, fr, jp
+  description: 'Bienvenido a mi portfolio', // e.g: Welcome to my website
+};
+
+// HERO DATA
+export const heroData = {
+  title: 'Mi nombre es',
+  name: 'Rodrigo',
+  subtitle: 'Soy estudiante de la carrera Analista Programador Universitario',
+  cta: 'Más sobre mí',
+};
+
+// ABOUT DATA
+export const aboutData = {
+  img: 'profile.jpg',
+  paragraphOne: 'Vivo en Comodoro Rivadavia (Chubut) junto a mi pareja.',
+  paragraphTwo: 'Actualmente curso el tercer y último año de la carrera Analista Programador Universitario en la Universidad Nacional de la Patagonia San Juan Bosco que se encuentra en esta ciudad.',
+  paragraphThree: 'Busco mi primer trabajo bajo relación de dependencia como programador full stack.',
+  resume: 'https://www.resumemaker.online/es.php', // if no resume, the button will not show up
+};
+
+// PROJECTS DATA
+export const projectsData = [
+  {
+    id: nanoid(),
+    img: 'garyYcris.jpg',
+    title: 'Gary & Cris',
+    info: 'Blog de la pareja',
+    info2: '',
+    url: 'https://7gary7.github.io/Gary-Cris/',
+    repo: 'https://github.com/7gary7/Gary-Cris', // if no repo, the button will not show up
+  },
+  {
+    id: nanoid(),
+    img: 'pipperman.jpg',
+    title: 'Pipperman',
+    info: 'Juego en desarrollo',
+    info2: 'Creado con Java utilizando el patrón Modelo-Vista-Controlador',
+    url: '',
+    repo: 'https://eljuegito.sourceforge.io', // if no repo, the button will not show up
+  },
+];
+
+// CONTACT DATA
+export const contactData = {
+  cta: '¿Quisieras contactarme? Escribe un email!',
+  btn: 'Contactame',
+  email: 'rodri_25193@hotmail.com',
+};
+
+// FOOTER DATA
+export const footerData = {
+  networks: [
+    {
+      id: nanoid(),
+      name: 'whatsapp',
+      url: 'wa.link/rp8vho',
+    },
+    {
+      id: nanoid(),
+      name: 'linkedin',
+      url: 'https://www.linkedin.com/in/rodrigo-garay-b589bb100/',
+    },
+    {
+      id: nanoid(),
+      name: 'github',
+      url: 'github.com/7gary7/',
+    },
+  ],
+};
+
+// Github start/fork buttons
+export const githubButtons = {
+  isEnabled: false, // set to false to disable the GitHub stars/fork buttons
+};
